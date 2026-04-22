b6de74f78428ca6f50cca8ceaddc3afa1f58bea7
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-04-22 14:39:09 -0600

    Added comments to the Hello World program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is a comment in C#. It's just like a comment in C
+        /* This is a block comment
+        you can add multiple lines to the comment
+        and close it with */
         Console.WriteLine("Hello Sandbox World!");
     }
 }
