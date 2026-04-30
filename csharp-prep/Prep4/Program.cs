920a2e376b1f34de6975f2861cbf948d5cf483d4
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-04-30 12:09:05 -0600

    Finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+        int numberInput = 1;
+        Console.WriteLine("Input a list of numbers. Type 0 when finished.");
+
+        while(numberInput != 0)
+        {
+            Console.Write("Input a number: ");
+            numberInput = int.Parse(Console.ReadLine());
+
+            if (numberInput != 0)
+            {
+                numberList.Add(numberInput);
+            }
+        }
+
+        int sum = numberList.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numberList.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int maximum = numberList.Max();
+        Console.WriteLine($"The largest number is: {maximum}");
+
+        int minPositive = numberList.FindAll(number => number > 0).Min();
+        Console.WriteLine($"The smallest positive number is {minPositive}");
+
+        numberList.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numberList)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
