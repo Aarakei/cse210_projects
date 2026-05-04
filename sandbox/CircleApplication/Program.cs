37de75c10105d87bcf82328353a0cabce99c7eda
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-04 14:43:17 -0600

    Added Circle class and code to construct it and call its methods in main

--- sandbox/CircleApplication/Program.cs
+++ sandbox/CircleApplication/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello World!");
+        Circle myCircle = new Circle();
+
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetArea());
+    }
+}
