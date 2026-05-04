9451e9c372807c823ad4d33ae3f5a293469cf0f7
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-04 14:59:51 -0600

    Added cylinder class

--- sandbox/CircleApplication/Program.cs
+++ sandbox/CircleApplication/Program.cs
@@ -7,5 +7,11 @@
 
         myCircle.SetRadius(10);
         Console.WriteLine(myCircle.GetArea());
+
+        Cylinder myCylinder = new Cylinder();
+        myCylinder._circle = new Circle();
+        myCylinder._circle.SetRadius(10);
+        myCylinder._height = 30;
+        Console.WriteLine(myCylinder.GetCylinderVolume());
     }
 }
