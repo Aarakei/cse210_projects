c1f2213aee7275eef4d1401b6e6de457392bd115
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-11 14:17:23 -0600

    messed with lists in the sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,6 +2,14 @@ using System;
 
 class Program
 {
+    static void PrintIntList(List<int> list)
+    {
+        Console.WriteLine("Int List:");
+        foreach(int i in list)
+        {
+            Console.WriteLine($"[{list.IndexOf(i)}]: {i}");
+        }
+    }
     static void Main(string[] args)
     {
         // This is a comment in C#. It's just like a comment in C
@@ -11,10 +19,35 @@ class Program
         // Console.WriteLine("Hello Sandbox World!");
 
 
-        for(int i=0; i<=65; i++)
-        {
-            long x = (long)Math.Pow(2,i);
-            Console.WriteLine($"2^{i}: {x}");
-        }
+        List<int> intList = new List<int>();
+        List<float> floatList = new List<float>();
+        List<int> anothaList = new List<int>();
+        anothaList = [1,2,3];
+
+        floatList.Add((float)3.14159);
+        floatList.Add((float)2.71828);
+
+        float e_plus_pi = floatList[0] + floatList[1];
+        intList.Add(5);
+
+        Console.WriteLine($"intList[0] = {intList[0]}");
+        Console.WriteLine($"e plus pi  = {e_plus_pi}");
+
+        intList.Insert(0,257);
+        intList.Insert(1,15);
+
+        PrintIntList(intList);
+
+        intList.Remove(15);
+        intList.RemoveAt(1);
+
+        PrintIntList(intList);
+
+        int num_1 = 3;
+        int num_2 = 1;
+
+        int result = num_1 & num_2;
+
+        Console.WriteLine(result);
     }
 }
