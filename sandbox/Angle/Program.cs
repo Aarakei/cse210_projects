710100d9045c73f8be9f2f14ae9d4d4c73234fc8
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-18 21:43:09 -0600

    added encapsulation Angle project to sandbox

---
+++ sandbox/Angle/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Angle World");
+
+        Angle myAngle = new Angle();
+        myAngle.SetRadians(5);
+        Console.WriteLine(myAngle.GetRadians());
+    }
+}
