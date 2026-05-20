bfd9c797c3cb2ee0c686a1b7ff51895d0a22ac5b
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-20 14:16:31 -0600

    Added a user input schema for scripture memorization program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string response = "";
+
+        while(response.ToLower() != "quit")
+        {
+            // Clear the screen
+            Console.Clear();
+
+            // TODO: print the scripture
+            Console.WriteLine("Proverbs 3:5-6: Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him and he shall direct thy paths.");
+            Console.WriteLine();
+            
+            // TODO: hide a few random words
+
+            // Get continue/quit input from user
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            response = Console.ReadLine();
+        }
     }
 }
