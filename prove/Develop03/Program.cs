3dd2cc11c4633cd41fe5b920010212f48867a308
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-20 14:42:36 -0600

    changed up the input layout

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,10 +6,13 @@ class Program
     {
         string response = "";
 
-        while(response.ToLower() != "quit")
+        while(response.ToLower() != "q")
         {
             // Clear the screen
             Console.Clear();
+
+            // Print context
+            Console.WriteLine("Enter 'q' to quit, press enter to remove words:\n");
 
             // TODO: print the scripture
             Console.WriteLine("Proverbs 3:5-6: Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him and he shall direct thy paths.");
@@ -18,7 +21,7 @@ class Program
             // TODO: hide a few random words
 
             // Get continue/quit input from user
-            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            Console.Write(" > ");
             response = Console.ReadLine();
         }
     }
