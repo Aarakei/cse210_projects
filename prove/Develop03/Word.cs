171c342d05a6c94a4cd2f0e5da83da930417e577
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-05-27 14:41:00 -0600

    removed unnecessary _hidden attribute from Word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,12 +1,10 @@
 class Word
 {
     private string _word;
-    private bool _hidden;
 
     public Word(string word)
     {
         _word = word;
-        _hidden = false;
     }
 
     public string GetWord()
