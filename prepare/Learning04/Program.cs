9960fd73faaadc2198346de185aa70e43d9123d7
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-06-01 17:42:59 -0600

    Wrote and tested the WritingAssignment class, finishing prepare 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,7 +5,12 @@ class Program
     static void Main(string[] args)
     {
         MathAssignment myMathAssignment = new MathAssignment("2.1", "1-21", "Bob Bobbertson", "Differential Equations");
+        WritingAssignment myWritingAssignment = new WritingAssignment("Effect of Phoenetic Spelling on the Naming of Countries", "Bob Bobbertson", "English 101");
+
         Console.WriteLine(myMathAssignment.GetSummary());
         Console.WriteLine(myMathAssignment.GetHomeworkList());
+
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
