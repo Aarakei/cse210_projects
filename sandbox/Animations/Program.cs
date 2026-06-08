ffe2e5f12308a3f086b00a6393527680f091c456
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-06-08 17:03:51 -0600

    In-class spinner animation activity

---
+++ sandbox/Animations/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Hide the cursor
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        // Make space for the blinker
+        int sleepTime = 250;
+        int blinkSteps = 10;
+        Console.Write(" ");
+
+        // Blink the blinker
+        for (int i = 0; i < blinkSteps; i++)
+        {
+            Console.Write("\b+");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b-");
+            Thread.Sleep(sleepTime);
+        }
+
+        // Erase the blinker
+        Console.Write("\b ");
+
+        // Give the cursor back
+        Console.CursorVisible = true;
+    }
+}
