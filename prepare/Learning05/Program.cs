1c30882bc555b7033fdedec74f0bf0b613302485
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-06-15 22:51:12 -0600

    did the polymorphism prepare activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("red", 4);
+        Console.WriteLine($"Color: {mySquare.GetColor()}");
+        Console.WriteLine($"Area: {mySquare.GetArea()}");
+
+        Rectangle myRectangle = new Rectangle("blue", 5, 4);
+        Console.WriteLine($"Color: {myRectangle.GetColor()}");
+        Console.WriteLine($"Area: {myRectangle.GetArea()}");
+
+        Circle myCircle = new Circle("green", 3);
+        Console.WriteLine($"Color: {myCircle.GetColor()}");
+        Console.WriteLine($"Area: {myCircle.GetArea()}");
     }
 }
