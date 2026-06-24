4d64557a53a27a6e66a53826eb9bee4f6475df12
Aarakei <52719110+Aarakei@users.noreply.github.com>
2026-06-24 14:43:38 -0600

    Get the base goal running

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop05 World!");
+        Quest myQuest = new Quest();
+        myQuest.SetName();
+        myQuest.SetDescription();
+        myQuest.SetPointValue();
+        Console.WriteLine(myQuest.GetDisplayString());
+        myQuest.CompleteQuest();
+        Console.WriteLine(myQuest.GetDisplayString());
     }
 }
---
+++ prove/Develop05/Quest.cs
@@ -0,0 +1,54 @@
+class Quest
+{
+    protected string _type;
+    protected string _name;
+    protected string _description;
+    protected int _pointValue;
+    protected int _timesCompleted;
+    protected bool _isCompleted;
+
+    public Quest()
+    {
+        _type = "";
+        _name = "";
+        _description = "";
+        _pointValue = 0;
+        _timesCompleted = 0;
+        _isCompleted = false;
+    }
+    public Quest(string name, string description, int pointValue)
+    {
+        _name = name;
+        _description = description;
+        _pointValue = pointValue;
+    }
+
+    public void SetName()
+    {
+        Console.Write("What is the name of the quest: ");
+        _name = Console.ReadLine();
+    }
+
+    public void SetDescription()
+    {
+        Console.Write($"Enter the description for {_name} quest: ");
+        _description = Console.ReadLine();
+    }
+
+    public void SetPointValue()
+    {
+        Console.Write($"Enter the points earned for {_name} quest: ");
+        _pointValue = int.Parse(Console.ReadLine());
+    }
+
+    public virtual string GetDisplayString()
+    {
+        return $"[{(_isCompleted ? "X" : " ")}] {_name}: {_description}";
+    }
+
+    public int CompleteQuest()
+    {
+        _isCompleted = true;
+        return _pointValue;
+    }
+}
